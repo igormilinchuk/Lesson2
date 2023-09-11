6e2eaa8ca6cb34525156b147a6827fff99a80a13
igormilinchuk <144233134+igormilinchuk@users.noreply.github.com>
2023-09-11 18:41:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+let boolean = true;
+let string = "best";
+let nul = NaN;
+let underf = undefined;
+let number = 190;
+
+console.table({ boolean, string, nul, underf, number });
+
+let information = 0;
+
+mydate(information);
+
+function mydate() {
+    let nickname = "Milinchuk";
+    let forename = "Igor";
+    let old = 17;
+    let place = "IvanoFrankivsk";
+    console.table({ nickname, forename, old, place });
+}
